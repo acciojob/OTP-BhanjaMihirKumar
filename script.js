41b9928e983437ae0c0fea5c68d0136382292b24
Mihir kumar Bhanja <139785978+BhanjaMihirKumar@users.noreply.github.com>
2026-09-23 00:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((input, index) => {
+
+    input.addEventListener("input", (e) => {
+
+        // Allow only numbers
+        input.value = input.value.replace(/[^0-9]/g, "");
+
+        // Move to next input
+        if (input.value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+
+        if (e.key === "Backspace") {
+
+            // Delete current value
+            input.value = "";
+
+            // Move to previous input
+            if (index > 0) {
+                codes[index - 1].focus();
+            }
+
+        }
+    });
+});
